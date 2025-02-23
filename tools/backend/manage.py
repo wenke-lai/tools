888508ce511a452e4c1c6cc805774cd3905268df
wenke.lai <wenke.studio@gmail.com>
2025-02-23 22:10:54 +0800

    fix: setup structlog configure

--- tools/backend/manage.py
+++ tools/backend/manage.py
@@ -1,9 +1,25 @@
+import structlog
 from dotenv import load_dotenv
 from fastapi import FastAPI
+from structlog.processors import CallsiteParameter, CallsiteParameterAdder, JSONRenderer
 
 from . import authentication, hello
 
 load_dotenv()
+
+structlog.configure(
+    processors=[
+        CallsiteParameterAdder(
+            {
+                CallsiteParameter.FILENAME,
+                CallsiteParameter.FUNC_NAME,
+                CallsiteParameter.LINENO,
+            }
+        ),
+        JSONRenderer(),
+    ],
+    # logger_factory=structlog.stdlib.LoggerFactory(),
+)
 
 
 def launch_backend(api: FastAPI):
