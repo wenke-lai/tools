f55c847de3b72ee85da3c6ed59e576258e488290
wenke.lai <wenke.studio@gmail.com>
2025-02-24 21:04:25 +0800

    fix(routes): add debug DOM to test /auth/login API

--- tools/frontend/routes/index.py
+++ tools/frontend/routes/index.py
@@ -1,3 +1,4 @@
+import httpx
 import reflex as rx
 
 from rxconfig import config
@@ -8,7 +9,20 @@ from .layout import layout
 class State(rx.State):
     """The app state."""
 
-    ...
+    session: str = rx.Cookie(name="__session")
+    access_token: str = rx.Cookie(name="access_token")
+
+    data: dict = {}
+
+    @rx.event(background=True)
+    async def auth(self):
+        async with self:
+            url = "http://localhost:8000/auth/login"
+            async with httpx.AsyncClient() as client:
+                response = await client.post(url, cookies={"__session": self.session})
+                print("---", response.cookies)
+                self.access_token = response.cookies["access_token"]
+                self.data = response.json()
 
 
 @layout
@@ -26,6 +40,16 @@ def index() -> rx.Component:
             is_external=True,
         ),
         rx.button("dashboard", on_click=rx.redirect("/dashboard")),
+        # todo: remove me, debug only
+        rx.box(
+            rx.text(
+                State.session,
+                class_name="text-3xl text-red-500 max-w-3xl max-h-32 overflow-auto",
+            ),
+            rx.button("auth", on_click=State.auth),
+            rx.text(State.data),
+            class_name="border border-sky-400 p-4 rounded space-y-4",
+        ),
         spacing="5",
         justify="center",
         min_height="85vh",
